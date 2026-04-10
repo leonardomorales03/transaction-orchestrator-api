c2b0c2c34e57ecefce2584930ded1baafe118a6c
leomorales <ingeniero.leonardo.morales@gmail.com>
2026-04-10 15:23:37 -0500

    chore: setup spring boot project and hexagonal architecture skeleton

---
+++ src/main/java/com/orchestrator/transaction/TransactionOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.orchestrator.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionOrchestratorApplication.class, args);
+    }
+
+}
